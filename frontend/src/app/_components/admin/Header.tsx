0460549e326d182a8e629cf849f3c9fce8388391
Ahmdpolash <ahmedpolash732@gmail.com>
2025-04-14 14:50:28 +0600

    added user avatar on header

--- frontend/src/app/_components/admin/Header.tsx
+++ frontend/src/app/_components/admin/Header.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -15,8 +17,14 @@ import { Separator } from "@/components/ui/separator";
 import BreadCrumbs from "./BreadCrumbs";
 import ThemeToggle from "../ThemeToggle";
 import Notification from "./Notification";
+import { useAppSelector } from "@/redux/hooks";
 
 const Header = () => {
+  const { user } = useAppSelector(
+    (state: { auth: { user: { avatar?: { url?: string } } | null } }) =>
+      state.auth
+  );
+
   return (
     <header className="sticky bg-white dark:bg-[#101828] top-0 z-30 border-b dark:border-slate-700 border-slate-300 shadow-sm flex h-16 shrink-0 items-center justify-between gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
       <div className="flex items-center gap-2 px-4">
@@ -32,34 +40,19 @@ const Header = () => {
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <Button variant="ghost" className="flex items-center gap-2">
-              <Avatar className="h-8 w-8">
-                <AvatarImage src="/placeholder.svg" alt="User" />
+              <Avatar className="h-10 w-10">
+                <AvatarImage src={user?.avatar?.url} alt="User" />
                 <AvatarFallback>AJ</AvatarFallback>
               </Avatar>
-              <span className="hidden md:inline-block font-medium">
-                Alex Johnson
-              </span>
-              <ChevronDown className="h-4 w-4" />
             </Button>
           </DropdownMenuTrigger>
           <DropdownMenuContent align="end" className="w-56">
             <DropdownMenuLabel>My Account</DropdownMenuLabel>
             <DropdownMenuSeparator />
-            <DropdownMenuItem asChild>
-              <Link href="/dashboard/profile">
+            <DropdownMenuItem asChild className="cursor-pointer">
+              <Link href="/dashboard/my-profile">
                 <User className="mr-2 h-4 w-4" />
                 <span>Profile</span>
-              </Link>
-            </DropdownMenuItem>
-
-            <DropdownMenuSeparator />
-            <DropdownMenuItem asChild>
-              <Link
-                href="/logout"
-                className="text-red-500 hover:text-red-600 dark:text-red-400 dark:hover:text-red-300"
-              >
-                <User className="mr-2 h-4 w-4" />
-                <span>Logout</span>
               </Link>
             </DropdownMenuItem>
           </DropdownMenuContent>
