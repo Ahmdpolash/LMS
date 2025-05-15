412568408f933c1f577d5482c1c8a5e5c9e0c622
Ahmdpolash <ahmedpolash732@gmail.com>
2025-05-15 21:23:39 +0600

    optimize the featured course

--- frontend/src/components/home/FeaturedCourse.tsx
+++ frontend/src/components/home/FeaturedCourse.tsx
@@ -7,21 +7,16 @@ import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import Container from "../shared/Container";
 import SectionHeaders from "./SectionHeaders";
-import { courses } from "@/constant";
+
 import { motion } from "framer-motion";
 import { useGetAllCoursesQuery } from "@/redux/features/course/courseApi";
 import Link from "next/link";
+import CardSkeleton from "../shared/CardSkeleton";
+import { redirect } from "next/navigation";
 export default function FeaturedCourse() {
   const { data, isLoading } = useGetAllCoursesQuery({});
-  const [courseData, setCourseData] = useState([]);
-
-  // const allCoursesData = data?.data?.courseData.map((course: any) =>
-  //   setCourseData(course)
-  // );
 
   const [hoveredCard, setHoveredCard] = useState<number | null>(null);
-
-  console.log(data);
 
   // Function to render stars based on rating
   const renderStars = (rating: number) => {
@@ -70,124 +65,138 @@ export default function FeaturedCourse() {
           />
 
           {/* Course Cards Grid */}
-          <motion.div
-            initial={{ opacity: 0, y: 40 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.4 }}
-            className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"
-          >
-            {data?.data?.map((course: any) => (
-              <Link key={course._id} href={`/courses/${course._id}`}>
-                <motion.div
-                  className="cursor-pointer bg-white dark:bg-[#1a2342] rounded-xl overflow-hidden shadow-sm border border-gray-300 dark:border-gray-800 hover:shadow-lg transition-all duration-300 flex flex-col lg:h-[460px]"
-                  onMouseEnter={() => setHoveredCard(course._id)}
-                  onMouseLeave={() => setHoveredCard(null)}
-                >
-                  {/* Course Image */}
-                  <div className="relative overflow-hidden">
-                    <Image
-                      src={course?.thumbnail?.url}
-                      alt={course?.name}
-                      width={350}
-                      height={200}
-                      className="w-full h-48 object-cover transition-transform duration-500"
-                      style={{
-                        transform:
-                          hoveredCard === course._id
-                            ? "scale(1.05)"
-                            : "scale(1)",
-                      }}
-                    />
-                    <div className="absolute top-3 left-3">
-                      <Badge className="bg-[rgb(37,150,190)] text-white">
-                        {course?.category}
-                      </Badge>
-                    </div>
-                    <div className="absolute top-3 right-3">
-                      <Badge className="bg-gray-900/80 text-white">
-                        {course?.level}
-                      </Badge>
-                    </div>
-                  </div>
-
-                  {/* Course Content */}
-                  <div className="p-5 flex flex-col flex-1 justify-between">
-                    <h3 className="font-semibold text-lg text-gray-900 dark:text-white mb-2 line-clamp-2">
-                      {course?.name}
-                    </h3>
-
-                    {/* Instructor */}
-                    <p className="text-gray-600 dark:text-gray-400 text-sm mb-3">
-                      By Elearning
-                    </p>
-
-                    {/* Rating */}
-                    <div className="flex items-center mb-2">
-                      <div className="flex mr-2">
-                        {renderStars(course?.ratings)}
+          {isLoading ? (
+            <CardSkeleton />
+          ) : (
+            <motion.div
+              initial={{ opacity: 0, y: 40 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.4 }}
+              className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"
+            >
+              {data?.data?.length > 0 ? (
+                data?.data?.map((course: any) => (
+                  <Link key={course._id} href={`/courses/${course._id}`}>
+                    <motion.div
+                      className="cursor-pointer bg-white dark:bg-[#1a2342] rounded-xl overflow-hidden shadow-sm border border-gray-300 dark:border-gray-800 hover:shadow-lg transition-all duration-300 flex flex-col lg:h-[460px]"
+                      onMouseEnter={() => setHoveredCard(course._id)}
+                      onMouseLeave={() => setHoveredCard(null)}
+                    >
+                      {/* Course Image */}
+                      <div className="relative overflow-hidden">
+                        <Image
+                          src={course?.thumbnail?.url}
+                          alt={course?.name}
+                          width={350}
+                          height={200}
+                          className="w-full h-48 object-cover transition-transform duration-500"
+                          style={{
+                            transform:
+                              hoveredCard === course._id
+                                ? "scale(1.05)"
+                                : "scale(1)",
+                          }}
+                        />
+                        <div className="absolute top-3 left-3">
+                          <Badge className="bg-[rgb(37,150,190)] text-white">
+                            {course?.category}
+                          </Badge>
+                        </div>
+                        <div className="absolute top-3 right-3">
+                          <Badge className="bg-gray-900/80 text-white">
+                            {course?.level}
+                          </Badge>
+                        </div>
                       </div>
 
-                      <span className="text-gray-500 dark:text-gray-400 text-sm ml-1">
-                        ({course.ratings} reviews)
-                      </span>
-                    </div>
+                      {/* Course Content */}
+                      <div className="p-5 flex flex-col flex-1 justify-between">
+                        <h3 className="font-semibold text-lg text-gray-900 dark:text-white mb-2 line-clamp-2">
+                          {course?.name}
+                        </h3>
 
-                    {/* Price and Stats */}
-                    <div className="flex-1">
-                      <div className="flex items-center justify-between ">
-                        <div>
-                          {course?.estimatedPrice ? (
-                            <div className="flex items-center">
-                              <span className="text-[rgb(37,150,190)] font-bold text-xl">
-                                ${course.price}
-                              </span>
-                              <span className="text-gray-500 dark:text-gray-400 line-through ml-2">
-                                ${course?.estimatedPrice}
-                              </span>
+                        {/* Instructor */}
+                        <p className="text-gray-600 dark:text-gray-400 text-sm mb-3">
+                          By Elearning
+                        </p>
+
+                        {/* Rating */}
+                        <div className="flex items-center mb-2">
+                          <div className="flex mr-2">
+                            {renderStars(course?.ratings)}
+                          </div>
+
+                          <span className="text-gray-500 dark:text-gray-400 text-sm ml-1">
+                            ({course.ratings} reviews)
+                          </span>
+                        </div>
+
+                        {/* Price and Stats */}
+                        <div className="flex-1">
+                          <div className="flex items-center justify-between ">
+                            <div>
+                              {course?.estimatedPrice ? (
+                                <div className="flex items-center">
+                                  <span className="text-[rgb(37,150,190)] font-bold text-xl">
+                                    ${course.price}
+                                  </span>
+                                  <span className="text-gray-500 dark:text-gray-400 line-through ml-2">
+                                    ${course?.estimatedPrice}
+                                  </span>
+                                </div>
+                              ) : (
+                                <span className="text-[rgb(37,150,190)] font-bold text-xl">
+                                  ${course.price}
+                                </span>
+                              )}
                             </div>
-                          ) : (
-                            <span className="text-[rgb(37,150,190)] font-bold text-xl">
-                              ${course.price}
-                            </span>
-                          )}
+
+                            {/* Students & Lectures */}
+                            <div className="text-right space-y-2">
+                              <div className="flex items-center justify-end text-gray-600 dark:text-gray-300 text-sm">
+                                <Users className="h-4 w-4 mr-1" />
+                                {/* <span>{course.students.toLocaleString()}</span> */}
+                                <span>{course?.purchased}</span>
+                              </div>
+                              <div className="flex items-center justify-end text-gray-600 dark:text-gray-300 text-sm mt-1">
+                                <BookOpen className="h-4 w-4 mr-1" />
+                                <span>
+                                  {course?.courseData?.length} lectures
+                                </span>
+                              </div>
+                            </div>
+                          </div>
                         </div>
 
-                        {/* Students & Lectures */}
-                        <div className="text-right space-y-2">
-                          <div className="flex items-center justify-end text-gray-600 dark:text-gray-300 text-sm">
-                            <Users className="h-4 w-4 mr-1" />
-                            {/* <span>{course.students.toLocaleString()}</span> */}
-                            <span>{course?.purchased}</span>
-                          </div>
-                          <div className="flex items-center justify-end text-gray-600 dark:text-gray-300 text-sm mt-1">
-                            <BookOpen className="h-4 w-4 mr-1" />
-                            <span>{course?.courseData?.length} lectures</span>
-                          </div>
+                        {/* Button - Now in its own container at the bottom */}
+                        <div className="">
+                          <Button className="w-full bg-[rgb(37,150,190)] hover:bg-[rgb(37,150,190)]/80 text-white">
+                            Enroll Now
+                          </Button>
                         </div>
                       </div>
-                    </div>
-
-                    {/* Button - Now in its own container at the bottom */}
-                    <div className="">
-                      <Button className="w-full bg-[rgb(37,150,190)] hover:bg-[rgb(37,150,190)]/80 text-white">
-                        Enroll Now
-                      </Button>
-                    </div>
-                  </div>
-                </motion.div>
-              </Link>
-            ))}
-          </motion.div>
+                    </motion.div>
+                  </Link>
+                ))
+              ) : (
+                <div className=" ">
+                  <h3 className="">No Course Available</h3>
+                </div>
+              )}
+            </motion.div>
+          )}
 
           {/* View All Button */}
           <div className="mt-12 text-center ">
-            <Button
-              variant="outline"
-              className="cursor-pointer border-[rgb(37,150,190)] text-[rgb(37,150,190)] hover:bg-[rgb(37,150,190)]/10 group"
-            >
-              View All Courses
-              <ArrowRight className="ml-2 h-4 w-4 group-hover:translate-x-1 transition-transform" />
-            </Button>
+            <Link href="/courses">
+              <Button
+                variant="outline"
+                className="cursor-pointer border-[rgb(37,150,190)] text-[rgb(37,150,190)] hover:bg-[rgb(37,150,190)]/10 group"
+              >
+                View All Courses
+                <ArrowRight className="ml-2 h-4 w-4 group-hover:translate-x-1 transition-transform" />
+              </Button>
+            </Link>
           </div>
         </div>
       </Container>
---
+++ frontend/src/components/shared/CardSkeleton.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+const CardSkeleton = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {[1, 2, 3].map((card) => (
+        <div className="">
+          <div
+            key={card}
+            className="animate-pulse bg-white dark:bg-[#1a2342] rounded-xl overflow-hidden shadow-sm border border-gray-300 dark:border-gray-800 flex flex-col lg:h-[460px]"
+          >
+            <div className="relative overflow-hidden h-48 bg-gray-200"></div>
+            <div className="p-5 flex flex-col flex-1 justify-between">
+              <div className="h-6 bg-gray-200 mb-2"></div>
+              <div className="h-4 bg-gray-200 mb-3"></div>
+              <div className="flex items-center mb-2">
+                <div className="h-4 bg-gray-200 w-20 mr-2"></div>
+                <div className="h-4 bg-gray-200 w-16 ml-1"></div>
+              </div>
+              <div className="flex-1">
+                <div className="flex items-center justify-between ">
+                  <div>
+                    <div className="flex items-center">
+                      <span className="h-8 bg-gray-200 w-10 block"></span>
+                      <span className="h-4 bg-gray-200 w-8 block ml-2"></span>
+                    </div>
+                  </div>
+                  <div className="text-right space-y-2">
+                    <div className="flex items-center justify-end text-gray-600 dark:text-gray-300 text-sm">
+                      <div className="h-4 bg-gray-200 w-16 block"></div>
+                    </div>
+                    <div className="flex items-center justify-end text-gray-600 dark:text-gray-300 text-sm mt-1">
+                      <div className="h-4 bg-gray-200 w-16 block"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="h-10 bg-gray-200 block"></div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CardSkeleton;
